befd2f72b9b9b305078eff346733e0d5eadebbca
ducmanh272001 <91021358+ducmanh272001@users.noreply.github.com>
2021-10-01 09:35:36 +0700

    upload

---
+++ include/footer.php
@@ -0,0 +1,175 @@
+
+	<!-- footer -->
+	<footer>
+		<div class="footer-top-first">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<!-- footer first section -->
+				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi</h2>
+				<p class="footer-main mb-4">
+				Nếu bạn đang cân nhắc một chiếc máy tính xách tay mới, đang tìm kiếm một dàn âm thanh nổi trên xe hơi mới mạnh mẽ hoặc mua một chiếc HDTV mới, chúng tôi sẽ giúp bạn dễ dàng tìm thấy chính xác những gì bạn cần với mức giá bạn có thể mua được. Chúng tôi cung cấp Giá thấp hàng ngày trên TV, máy tính xách tay, điện thoại di động, máy tính bảng và iPad, trò chơi điện tử, máy tính để bàn, máy ảnh và máy quay phim, âm thanh, video và hơn thế nữa..</p>
+				<!-- //footer first section -->
+				<!-- footer second section -->
+				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Miễn phí vận chuyển</h3>
+								<p>Đơn hàng trên 100$</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Vận chuyển nhanh</h3>
+								<p>Toàn quốc</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Tin cậy</h3>
+								<p>Sự đảm bảo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		<div class="w3l-middlefooter-sec">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<div class="row footer-info w3-agileits-info">
+					<!-- footer categories -->
+					<div class="col-md-3 col-sm-6 footer-grids">
+						<h3 class="text-white font-weight-bold mb-3">Thể loại</h3>
+						<ul>
+							<li class="mb-3">
+								<a href="product.html">Điện thoại</a>
+							</li>
+							<li class="mb-3">
+								<a href="product.html">Máy tính</a>
+							</li>
+							<li class="mb-3">
+								<a href="product.html">TV, Audio</a>
+							</li>
+							<li class="mb-3">
+								<a href="product2.html">Điện thoại thông minh</a>
+							</li>
+							<li class="mb-3">
+								<a href="product.html">Máy giặt</a>
+							</li>
+							<li>
+								<a href="product2.html">Tủ lạnh</a>
+							</li>
+						</ul>
+					</div>
+					<!-- //footer categories -->
+					<!-- quick links -->
+					<div class="col-md-3 col-sm-6 footer-grids mt-sm-0 mt-4">
+						<h3 class="text-white font-weight-bold mb-3">Đường dẫn nhanh</h3>
+						<ul>
+							<li class="mb-3">
+								<a href="">Về chúng tôi</a>
+							</li>
+							<li class="mb-3">
+								<a href="contact.html">Liên hệ chúng tôi</a>
+							</li>
+							<li class="mb-3">
+								<a href="">Trợ giúp</a>
+							</li>
+							<li class="mb-3">
+								<a href="">Câu hỏi thường gặp</a>
+							</li>
+							<li class="mb-3">
+								<a href="">Điều khoản sử dụng</a>
+							</li>
+							<li>
+								<a href="">Chính sách bảo mật</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-3 col-sm-6 footer-grids mt-md-0 mt-4">
+						<h3 class="text-white font-weight-bold mb-3">Liên lạc</h3>
+						<ul>
+							<li class="mb-3">
+								<i class="fas fa-map-marker"></i> 123 Sebastian, USA.</li>
+							<li class="mb-3">
+								<i class="fas fa-mobile"></i> 0969 309 708 </li>
+							<li class="mb-3">
+								<i class="fas fa-phone"></i> 0969 309 708 </li>
+							<li class="mb-3">
+								<i class="fas fa-envelope-open"></i>
+								<a href="mailto:example@mail.com"> anhcubom1402@gmail.com</a>
+							</li>
+							<li>
+								<i class="fas fa-envelope-open"></i>
+								<a href="mailto:example@mail.com"> doducmanh2001@gmail.com</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-3 col-sm-6 footer-grids w3l-agileits mt-md-0 mt-4">
+						<!-- newsletter -->
+						<h3 class="text-white font-weight-bold mb-3">Bản tin</h3>
+						<p class="mb-3">Giao hàng miễn phí cho đơn hàng đầu tiên của bạn!</p>
+						<form action="#" method="post">
+							<div class="form-group">
+								<input type="email" class="form-control" placeholder="Email" name="email" required="">
+								<input type="submit" value="Go">
+							</div>
+						</form>
+						<!-- //newsletter -->
+						<!-- social icons -->
+						<div class="footer-grids  w3l-socialmk mt-3">
+							<h3 class="text-white font-weight-bold mb-3">Follow Us on</h3>
+							<div class="social">
+								<ul>
+									<li>
+										<a class="icon fb" href="#">
+											<i class="fab fa-facebook-f"></i>
+										</a>
+									</li>
+									<li>
+										<a class="icon tw" href="#">
+											<i class="fab fa-twitter"></i>
+										</a>
+									</li>
+									<li>
+										<a class="icon gp" href="#">
+											<i class="fab fa-google-plus-g"></i>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<!-- //social icons -->
+					</div>
+				</div>
+				<!-- //quick links -->
+			</div>
+		</div>
+		
+		<!-- //footer third section -->
+		
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© 2018. Công ty cổ phần Thế Giới Di Động. GPDKKD: 0969309708 do sở KH & ĐT TP.Hà Nôi cấp ngày 02/01/2007. GPMXH: 238/GP-BTTTT do Bộ Thông Tin và Truyền Thông cấp ngày 04/06/2020.
+Địa chỉ: 212 Phạm Văn Đồng, Cầu Giấy, Q.1, Hà Nội. Điện thoại: 0969309708. Email: doducmanh0207.com.
+				<a href="http://w3layouts.com"></a>
+			</p>
+		</div>
+	</div>
+	<!-- //copyright -->
